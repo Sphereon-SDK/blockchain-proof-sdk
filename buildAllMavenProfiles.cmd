1184c2d4c14174129eebad15e173c845cd010e2d
Sander <spostma@sphereon.com>
2017-12-15 11:11:17 +0100

    - Use custom mustache template - Remove generated sources

--- buildAllMavenProfiles.cmd
+++ buildAllMavenProfiles.cmd
@@ -1,3 +1,16 @@
+cd %~dp0
+del java8-okhttp-gson\pom.xml
+del java8-okhttp-gson\build.gradle
+rmdir java8-okhttp-gson\src\main  /s /q
+del java8-jersey2\pom.xml
+del java8-jersey2\build.gradle
+rmdir java8-jersey2\src\main  /s /q
+del java8-retrofit2\pom.xml
+del java8-retrofit2\build.gradle
+rmdir java8-retrofit2\src\main  /s /q
+rmdir csharp-net35\src\Sphereon.SDK.Blockchain.Proof  /s /q
+rmdir csharp-net45\src\Sphereon.SDK.Blockchain.Proof  /s /q
+
 call mvn -Pjava8-jersey2 clean install
 start mvn -Pjava8-okhttp-gson clean install
 start mvn -Pjava8-retrofit2 clean install
