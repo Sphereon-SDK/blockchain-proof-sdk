e6be40d8b718da6a01241d84eb82b732f46ed40c
sanderPostma <spostma73@gmail.com>
2018-07-12 10:01:03 +0200

    Versioned Java SDK

--- buildAllMavenProfiles.cmd
+++ buildAllMavenProfiles.cmd
@@ -13,6 +13,7 @@ rmdir csharp-net45\src\Sphereon.SDK.Blockchain.Proof  /s /q
 
 call mvn -Pjava8-jersey2 clean install
 start mvn -Pjava8-okhttp-gson clean install
+start mvn -Pjava8-okhttp-gson_versioned clean install
 start mvn -Pjava8-retrofit2 clean install
 start mvn -Pcsharp-net45 clean install
 start mvn -Pcsharp-net35 clean install
